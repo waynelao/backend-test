cd671c3124e8720c62d269b0ea616e769b756e9c
waynelao <waynelao7@gmail.com>
2022-06-17 12:36:47 -0500

    update the api __init__.py

--- index/api/__init__.py
+++ index/api/__init__.py
@@ -1,3 +1,6 @@
 """backend-test REST API."""
 from index.api.main import index_url
 from index.api.main import index_shows
+from index.api.main import insert_shows
+from index.api.main import delete_shows
+from index.api.main import update_shows
